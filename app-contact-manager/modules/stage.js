b6240f6eee57cc903acccc7bd9b3e595d68e9439
Dragos Iordache <dragosh.iordache@yahoo.com>
2022-12-12 20:20:17 +0200

    finalzied contact app

--- app-contact-manager/modules/stage.js
+++ app-contact-manager/modules/stage.js
@@ -1,7 +1,13 @@
 import { addMessage, clearMessages } from './notificationBar.js';
-import { createContact, deleteContact, findContact } from './query.js';
+import {
+  createContact,
+  createPet,
+  deleteContact,
+  findContact,
+} from './query.js';
 import createMessage from './message.js';
 import { render as renderEditContact } from './editContact.js';
+import { render as renderPetForm } from './addPetForm.js';
 
 const stage = document.querySelector('.stage');
 
@@ -121,6 +127,57 @@ stage.addEventListener('submit', (event) => {
   clearMessages();
   addMessage(
     createMessage(`Contact ${contact.name} ${contact.surname} updated.`),
+  );
+});
+
+// add pet button
+stage.addEventListener('click', (event) => {
+  const { target } = event;
+
+  if (
+    target.nodeName !== 'BUTTON' ||
+    !target.classList.contains('add-pet-button')
+  ) {
+    return;
+  }
+
+  const button = target;
+  const parentElement = button.parentElement;
+  const contactId = parentElement.dataset.contactId;
+
+  // voi folositi metoda de la teme
+  stage.innerHTML = '';
+
+  stage.append(renderPetForm(contactId));
+});
+
+// create pet submit
+stage.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const { target } = event;
+
+  if (target.nodeName !== 'FORM' || !target.classList.contains('add-pet')) {
+    return;
+  }
+
+  const form = target;
+  // DOM input elements
+  const { name, species, age, contactId } = form;
+
+  createPet(contactId.value, {
+    name: name.value,
+    species: species.value,
+    age: age.value,
+    id: Number(Date.now().toString().slice(-6)),
+  });
+
+  // cum putem sa afisam pet created for contact Carol
+
+  // folositi metoda de clear stage
+  stage.innerHTML = '';
+
+  addMessage(
+    createMessage(`Pet ${name.value} created for contact ${contactId.value}`),
   );
 });
 
